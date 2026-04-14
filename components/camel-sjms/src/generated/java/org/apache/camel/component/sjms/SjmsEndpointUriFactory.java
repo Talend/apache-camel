1cf391cfc01041780c5ebd563253a52b7b2d839a
Andrea Cosentino <ancosen@gmail.com>
2026-04-14 14:57:18 +0200

    [backport camel-4.14.x] CAMEL-23321: Add deserialization filtering for JMS ObjectMessage handling in camel-jms and camel-sjms (#22604)
    * CAMEL-23321: Add deserialization filtering for JMS ObjectMessage handling in camel-jms and camel-sjms
    
    Aligns the JMS component family with the defense-in-depth pattern applied in
    camel-netty (CAMEL-23297) and camel-mina (CAMEL-23319).
    
    JmsBinding (camel-jms and camel-sjms) now resolves a deserialization filter at
    construction using the same priority as the other components:
      configured value -> JVM -Djdk.serialFilter -> default "java.**;javax.**;org.apache.camel.**;!*".
    After ObjectMessage.getObject(), the class of the returned payload is checked
    against the filter and a SecurityException is thrown on reject. The filter is
    exposed as a new endpoint option 'deserializationFilter' on JmsConfiguration
    (camel-jms) and SjmsEndpoint (camel-sjms).
    
    Note: this check runs after the JMS provider has deserialized the payload, so
    the option does not, on its own, stop gadget chains that execute inside the
    provider's ObjectInputStream. The javadoc makes this explicit and points users
    to JVM-wide -Djdk.serialFilter and provider-specific filters for complete
    coverage. camel-amqp inherits the behaviour via AMQPJmsBinding.
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    * Regen
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    * Regen
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    * CAMEL-23321: Add deserialization filtering for JMS ObjectMessage handling in camel-jms and camel-sjms
    
    Configure deserializationFilter in CxfToJmsInOutTest and JmsToCxfInOutTest
    Spring contexts to allow org.apache.cxf.** classes alongside the defaults,
    so MessageContentsList payloads exchanged over JMS ObjectMessage pass the
    filter introduced in CAMEL-23321.
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    * Regen
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    ---------
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-sjms/src/generated/java/org/apache/camel/component/sjms/SjmsEndpointUriFactory.java
+++ components/camel-sjms/src/generated/java/org/apache/camel/component/sjms/SjmsEndpointUriFactory.java
@@ -23,7 +23,7 @@ public class SjmsEndpointUriFactory extends org.apache.camel.support.component.E
     private static final Set<String> SECRET_PROPERTY_NAMES;
     private static final Set<String> MULTI_VALUE_PREFIXES;
     static {
-        Set<String> props = new HashSet<>(47);
+        Set<String> props = new HashSet<>(48);
         props.add("acknowledgementMode");
         props.add("allowNullBody");
         props.add("asyncConsumer");
