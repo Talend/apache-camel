fe805243bcb526092bdc64c8f604527995c92651
Andrea Cosentino <ancosen@gmail.com>
2026-05-05 09:23:18 +0200

    [backport camel-4.14.x] CAMEL-23373: camel-jms - Disable ObjectMessage by default (#22920)
    * CAMEL-23373: camel-jms - Disable ObjectMessage by default (#22866)
    
    * CAMEL-23373: camel-jms - Disable ObjectMessage by default
    
    Java object serialization over JMS is rarely used in practice and is a
    recurring source of security advisories. Disable creation and reading
    of jakarta.jms.ObjectMessage by default and add a new objectMessageEnabled
    option (default false) on JmsConfiguration / JmsComponent / JmsEndpoint
    to opt back in.
    
    When disabled, JmsBinding now refuses to:
    
    - create an ObjectMessage from a Serializable body (auto-detected
      jmsMessageType=Object or explicitly set)
    - create an ObjectMessage for transferExchange or transferException
    - extract the body of a received ObjectMessage
    
    Existing tests that rely on ObjectMessage have been updated to opt in.
    A new JmsObjectMessageEnabledTest validates the default disabled
    behavior on producer and consumer side, and JmsBindingTest covers the
    unit-level checks. Documentation updated in the 4.21 upgrade guide.
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    * CAMEL-23373: Regenerate camel-jms / camel-activemq* / camel-amqp DSL builders
    
    The componentdsl and endpointdsl builder factories for camel-jms and the
    JMS-derived components (camel-activemq, camel-activemq6, camel-amqp) needed
    to be regenerated to expose the new objectMessageEnabled option in the
    fluent DSL. These are generated files only; no hand-written code changed.
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    ---------
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    * CAMEL-23373: camel-jms - Disable ObjectMessage by default
    
    ---------
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    Co-authored-by: Claus Ibsen <claus.ibsen@gmail.com>

--- tests/camel-itest/src/test/java/org/apache/camel/itest/jms/JmsJaxbTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/jms/JmsJaxbTest.java
@@ -79,6 +79,7 @@ public class JmsJaxbTest extends CamelTestSupport {
     protected void bindToRegistry(Registry registry) {
         // add ActiveMQ with embedded broker
         JmsComponent amq = jmsServiceExtension.getComponent();
+        amq.setObjectMessageEnabled(true);
 
         amq.setCamelContext(context);
 
