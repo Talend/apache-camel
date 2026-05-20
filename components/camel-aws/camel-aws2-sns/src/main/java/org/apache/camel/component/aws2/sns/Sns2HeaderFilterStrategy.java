81e66bab36b103cff937cb4e697b84abd6a104e4
Andrea Cosentino <ancosen@gmail.com>
2026-05-20 15:01:47 +0200

    CAMEL-23506: align aws2-sqs/sns HeaderFilterStrategy with siblings (#23373)
    Sqs2HeaderFilterStrategy and Sns2HeaderFilterStrategy only configured
    an outbound filter. Add setInFilterStartsWith(CAMEL_FILTER_STARTS_WITH)
    so the inbound direction is aligned with the sibling strategies
    (Kafka, Mail, CoAP, Google Pub/Sub, ...). The existing outbound regex
    (which also catches breadcrumbId and org.apache.camel.*) is unchanged.
    
    Includes focused unit tests in both modules and the
    camel-4x-upgrade-guide-4_14.adoc note.
    
    (cherry picked from commit 5f57258fb33d33ef99df10594f8fe9f11d7c2b7e)
    
    JIRA: https://issues.apache.org/jira/browse/CAMEL-23506
    
    Closes #23221

--- components/camel-aws/camel-aws2-sns/src/main/java/org/apache/camel/component/aws2/sns/Sns2HeaderFilterStrategy.java
+++ components/camel-aws/camel-aws2-sns/src/main/java/org/apache/camel/component/aws2/sns/Sns2HeaderFilterStrategy.java
@@ -26,5 +26,6 @@ public class Sns2HeaderFilterStrategy extends DefaultHeaderFilterStrategy {
     protected void initialize() {
         // filter headers begin with "Camel" or "org.apache.camel"
         setOutFilterPattern("(breadcrumbId|Camel|org\\.apache\\.camel)[\\.|a-z|A-z|0-9]*");
+        setInFilterStartsWith(CAMEL_FILTER_STARTS_WITH);
     }
 }
